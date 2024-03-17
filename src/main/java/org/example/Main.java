836c68e22c54e65754fa56b9fca1fcbcf794f7d7
ashish <ashish135ranjan@gmail.com>
2024-03-17 12:16:07 +0530

    updated retrofit code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
         System.out.println("Hello");
         OkHttpClient client = new OkHttpClient();
@@ -41,25 +41,15 @@ public class Main {
 
         GitHubService service = retrofit.create(GitHubService.class);
         Call<List<Repo>> repos = service.listRepos("octocat");
-        repos.enqueue(new Callback<List<Repo>>() {
-            @Override
-            public void onResponse(Call<List<Repo>> call, retrofit2.Response<List<Repo>> response) {
-                if (response.isSuccessful()) {
-                    System.out.println("Response from GitHub API:");
-                    List<Repo> repoList = response.body();
-                    for (Repo repo : repoList) {
-                        System.out.println(repo);
-                    }
-                } else {
-                    System.out.println("Something went wrong with the Retrofit call!");
-                }
+        try {
+            List<Repo> repoList = repos.execute().body();
+            for (Repo repo : repoList) {
+                System.out.println(repo);
             }
-
-            @Override
-            public void onFailure(Call<List<Repo>> call, Throwable t) {
-                System.out.println("Network Error: " + t.getMessage());
-            }
-        });
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
 
