1b1d155c13d4989ea7c6b0d02018f5cbeb75d3a8
ashish <ashish135ranjan@gmail.com>
2024-03-17 12:19:19 +0530

    updated repo code

--- src/main/java/org/example/Repo.java
+++ src/main/java/org/example/Repo.java
@@ -1,8 +1,17 @@
 package org.example;
 
+
 public class Repo {
     private String id;
     private String name;
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
     @Override
     public String toString() {
